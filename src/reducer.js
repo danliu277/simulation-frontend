b6caea7571113647bd9ef04506747442819a123d
flatironschool <danliu277@gmail.com>
2020-11-03 14:26:01 -0500

    Add reducer

---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+let initialState = {
+}
+
+const reducer = (prevState = initialState, action) => {
+    switch (action.type) {
+        default:
+            return prevState
+    }
+}
+
+export default reducer;
