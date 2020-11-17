d258e074b5d64f4a3f3949863240b5205b96b575
flatironschool <danliu277@gmail.com>
2020-11-17 17:06:49 -0500

    Add login to routes

--- src/component/routes.js
+++ src/component/routes.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from '../App';
-import Login from '../container/Login';
 
 const Routes = () => {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/login" component={Login} />
-            </Switch>
+            <Route exact path="/" component={App} />
         </Router>
     )
 }
