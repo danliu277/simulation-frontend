08eb3a9968f93074001491c6ab7de6dce2a13b6d
flatironschool <danliu277@gmail.com>
2020-11-18 16:29:55 -0500

    Add state for username and password

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <div>
-        <form>
-          <label>Username</label>
-          <input type="text" name="username" />
-          <label>Password</label>
-          <input type="password" name="password" />
-          <input type="submit" value="login" />
-        </form>
+      <form>
+        <label>Username</label>
+        <input type="text" name="username" />
+        <label>Password</label>
+        <input type="password" name="password" />
+        <input type="submit" value="login" />
+      </form>
     </div>
   )
 }
