9f501a33873be23d1f69f8e7479f89db5357f011
flatironschool <danliu277@gmail.com>
2020-11-18 16:36:15 -0500

    Add onChange for username and password

--- src/container/Login.js
+++ src/container/Login.js
@@ -4,13 +4,21 @@ const Login = () => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
+  const handleUsername = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
   return (
     <div>
       <form>
         <label>Username</label>
-        <input type="text" name="username" />
+        <input type="text" name="username" value={username} onChange={handleUsername} />
         <label>Password</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" value={password} onChange={handlePassword} />
         <input type="submit" value="login" />
       </form>
     </div>
