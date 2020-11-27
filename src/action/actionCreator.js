5462e05e8c7c0e866b6a831f8c06b3be082c8edf
flatironschool <danliu277@gmail.com>
2020-11-27 13:44:50 -0500

    Create action for login and register

--- src/action/actionCreator.js
+++ src/action/actionCreator.js
@@ -1,1 +1,29 @@
 import { API_ROOT, HEADERS } from '../constants'
+
+export const setUserActionCreator = (user) => ({ type: 'SETUSER', user });
+
+export const registerActionCreator = (username, password) => {
+    return dispatch => {
+        fetch(`${API_ROOT}/users/create`, {
+            method: 'POST',
+            headers: HEADERS,
+            body: JSON.stringify({ username, password })
+        }).then(res => res.json())
+            .then(user => {
+                dispatch(setUserActionCreator(user))
+            })
+    }
+}
+
+export const loginActionCreator = (username, password) => {
+    return dispatch => {
+        fetch(`${API_ROOT}/users/login`, {
+            method: 'POST',
+            headers: HEADERS,
+            body: JSON.stringify({ username, password })
+        }).then(res => res.json())
+            .then(user => {
+                dispatch(setUserActionCreator(user))
+            })
+    }
+}
