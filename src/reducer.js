4f2c2d749ce8c221716de86b0c469606ad596557
flatironschool <danliu277@gmail.com>
2020-11-27 13:45:32 -0500

    Add user to store

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,11 @@
 let initialState = {
+    user: null
 }
 
 const reducer = (prevState = initialState, action) => {
     switch (action.type) {
+        case 'SETUSER':
+            return { ...prevState, user: action.user }
         default:
             return prevState
     }
